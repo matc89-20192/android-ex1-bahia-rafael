2651779157461ca2ba727de3edc8216f86cabcac
RAFAELDEOLIVEIRABAHIA <rafael.bahia@email.br>
2019-08-22 21:05:34 -0300

    Finalização da Atividade, após passar nos três testes solicitados

--- app/src/main/java/matc89/exercicio1/MainActivity.java
+++ app/src/main/java/matc89/exercicio1/MainActivity.java
@@ -2,12 +2,31 @@ package matc89.exercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String valor = "Mundo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView texto = (TextView) findViewById(R.id.labelMensagem);
+        texto.setText("Alô, " + MainActivity.valor + "!");
     }
+
+    public void changeText(View view){
+
+        EditText campoText = (EditText) findViewById(R.id.editNome);
+        MainActivity.valor = campoText.getText().toString();
+
+        TextView texto = (TextView) findViewById(R.id.labelMensagem);
+        texto.setText("Alô, " + valor + "!");
+    }
+
+
 }
